87490f0a01f094bcd554523d5bc225cf57dd43b1
Nicholas K <got2play9@gmail.com>
2017-05-23 00:02:56 -0700

    Random string setup

---
+++ RandomDateString/RandString.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomDateString
+{
+    public class RandString
+    {
+        int randNum = 0;
+
+        private int RandomNumCalc
+        {
+            get
+            {
+                string junk;
+                string something = "";
+                Byte randByte = 0x00000;
+                DateTime dt = DateTime.Now;
+
+                junk = something + Convert.ToString(randByte);
+                return randNum = (Convert.ToInt32(dt.ToBinary()) * Convert.ToInt32(Math.PI)) / Convert.ToInt32(junk);
+            }
+        }
+
+        private int RandomSeedTrue()
+        {
+            randNum = ((RandomNumCalc* RandomNumCalc) / RandomNumCalc) + RandomNumCalc- RandomNumCalc;
+            return randNum;
+        }
+    }
+}
